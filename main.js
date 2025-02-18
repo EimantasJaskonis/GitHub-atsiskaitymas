1c05188f8acf0738ab1afd19eb643e947d0aac01
EimantasJaskonis <eimantasjaskonis@gmail.com>
2025-02-18 15:55:11 +0200

    new JS

--- main.js
+++ main.js
@@ -2,18 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const tabButtons = document.querySelectorAll(".tab-button");
     const tabs = document.querySelectorAll(".tab");
     const closeButtons = document.querySelectorAll(".close-tab");
-
-    // Atidaryti tab'ą
     tabButtons.forEach(button => {
         button.addEventListener("click", (e) => {
             e.preventDefault();
-            const targetTab = document.getElementById(button.dataset.tab);
+            const targetTab = document.querySelector(`#${button.dataset.tab}`);
             tabs.forEach(tab => tab.style.display = "none");
             if(targetTab) targetTab.style.display = "block";
         });
     });
-
-    // Uždaryti tab'ą su animacija
     closeButtons.forEach(btn => {
         btn.addEventListener("click", (e) => {
             e.preventDefault();
